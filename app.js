805777559738779cc895c0e8c935586624524fc7
afride596 <afride596@gmail.com>
2024-08-26 01:15:33 +0530

    "Updated .gitignore, app.js, package-lock.json, and package.json files"

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-const root=ReactDom.createRoot(document.getElementById('root'))
-root.render(<App/>)
+// ReactElement
+const heading=(<h1>Afride is Hero🚀</h1>);
+// ReactComponent
+
+
+
+const root=ReactDom.createRoot(document.getElementById("root"));
+root.render(heading)
