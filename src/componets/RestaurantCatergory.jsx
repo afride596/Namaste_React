3032b78c7cd99cf41c1c0b45a745bad98732be7a
afride596 <afride596@gmail.com>
2024-09-19 04:06:10 +0530

    implemented toggle feature

--- src/componets/RestaurantCatergory.jsx
+++ src/componets/RestaurantCatergory.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ItemList from "./ItemList";
-const RestaurantCatergory = ({ Data }) => {
-  const [showitem, setshowitem] = useState(false);
+
+const RestaurantCatergory = ({ Data, showitem, setshowindex }) => {
+  const [localHideItem, setLocalHideItem] = useState(true); // initialize localHideItem to hideitem
+  // const [render, setRender] = useState(false);
 
   const handleClick = () => {
-    showitem===false?setshowitem(true):setshowitem(false)
+    setLocalHideItem(!localHideItem);
+    setshowindex();
   };
 
   return (
     <div>
       {/*herder */}
       <div className="w-[50%] mx-auto border-b-[2px] p-4 text-lg bg-[#ffffff] f items-center my-16 shadow-lg cursor-pointer rounded-lg">
-        <div className="flex justify-between tra " onClick={handleClick}>
+        <div className="flex justify-between tra" onClick={handleClick}>
           <span className="font-bold text-lg ">
             {Data.title} ({Data.itemCards.length})
           </span>
@@ -24,13 +27,12 @@ const RestaurantCatergory = ({ Data }) => {
             />
           </span>
         </div>
-        
-
-        {showitem && <ItemList Data={Data.itemCards} />}
-  
-        {/* {console.log(Data.itemCards)} */}
+        {localHideItem === false
+          ? localHideItem && <ItemList Data={Data.itemCards} />
+          : showitem && <ItemList Data={Data.itemCards} />}
       </div>
     </div>
   );
 };
+
 export default RestaurantCatergory;
--- src/componets/RestaurantsMenu.js
+++ src/componets/RestaurantsMenu.js
@@ -1,12 +1,14 @@
 import { useParams } from "react-router-dom";
 import useRestaurantsMenu from "../utils/useRestaurantsMenu";
 import RestaurantCatergory from "./RestaurantCatergory";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const RestaurantsMenu = () => {
   const { resId } = useParams();
 
   const resInfo = useRestaurantsMenu(resId);
+  const [showindex, setshowindex] = useState();
+ 
 
   if (resInfo === null) return <div>Loading...</div>;
 
@@ -20,9 +22,7 @@ const RestaurantsMenu = () => {
   // let { itemCards } =
   //   resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
 
-
   // console.log(resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
-
 
   const itemCategory =
     resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
@@ -33,17 +33,23 @@ const RestaurantsMenu = () => {
         );
       }
     );
-  // console.log(itemCategory);
+  console.log(itemCategory);
 
   return (
     <div className="text-center my-10">
       <h1 className="my-5 font-bold text-xl">{name}</h1>
       <h1 className="font-semibold">
-        {cuisines.join(",")}  -  {costForTwoMessage}
+        {cuisines.join(",")} - {costForTwoMessage}
       </h1>
-      {itemCategory.map((catergory) => (
-        <RestaurantCatergory  Data={catergory?.card?.card}  />
+      {itemCategory.map((catergory, index) => (
+        <RestaurantCatergory
+          key={catergory?.card?.card.title}
+          Data={catergory?.card?.card}
+          showitem={index === showindex ? true : false}
+          
+          setshowindex={() => setshowindex(index)}
         
+        />
       ))}
     </div>
   );
