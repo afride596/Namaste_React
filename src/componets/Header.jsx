80f63cd289ea42ef97d0ef4b442d561f6c272b38
afride596 <afride596@gmail.com>
2024-09-25 02:53:05 +0530

    final production

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
             </li>
             <li className="font-medium hover:text-gray-500">
           
-              <Link to="/grocery">rocery</Link>
+              <Link to="/grocery">Grocery</Link>
             </li>
 
             <div className="w-7 flex relative cursor-pointer">
